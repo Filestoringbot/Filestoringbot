cea95d9a95316c13dc4c469a103f83643294a92c
Filestoringbot <145536433+Filestoringbot@users.noreply.github.com>
2023-09-20 16:54:21 +0530

    Add files via upload

---
+++ server/exceptions.py
@@ -0,0 +1,12 @@
+    # Credit @LazyDeveloper.
+    # Please Don't remove credit.
+    # Born to make history @LazyDeveloper !
+
+    # Thank you LazyDeveloper for helping us in this Journey
+    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+    # thanks Adarsh Goel for Server Routing
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
